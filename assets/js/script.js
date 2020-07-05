76cf8debd4ef7b50e205159436be276495b08309
Waves <64664814+sanders43@users.noreply.github.com>
2020-07-05 12:57:43 -0500

    display date and time dynamically

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+let workDay = {
+    "8 AM": "",
+    "9 AM": "",
+    "10 AM": "",
+    "11 AM": "",
+    "12 PM": "",
+    "1 PM": "",
+    "2 PM": "",
+    "3 PM": "",
+    "4 PM": "",
+    "5 PM": "",
+}
+// Display Current Date and Time in Header
+
+let getDateAndTime = () => {
+    $("#currentDay").text(moment().format("dddd, MMMM Do YYYY, h:mm:ss a"))
+}
+getDateAndTime();
+// Update time without refreshing page
+setInterval(getDateAndTime, 1000);
